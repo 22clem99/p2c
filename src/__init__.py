ef051b88498cf26eb6fcbc6160cbabe5014dce94
Clément Harang <clement.harang22@gmail.com>
2025-08-20 21:22:38 +0200

    Continue to implement FSM for Project management

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from src import cli
+from src import input
+from src import output
+from src import node
+from src import project
--- src/fsm/FSMProjectManager.py
+++ src/fsm/FSMProjectManager.py
@@ -1,24 +1,40 @@
 from statemachine import State
 from statemachine import StateMachine
+from src.cli import Cmd, CmdProject
 
 class FSMProjectManager(StateMachine):
-    s_idle = State(initial=True)
-    s_create = State()
+    s_idle         = State(initial=True)
+    s_create       = State()
     s_open_project = State()
-    s_delete = State()
-    s_close = State()
-    s_active = State()
-    s_inactive = State()
+    s_delete       = State()
+    s_close        = State()
+    s_inactive     = State()
+    s_active       = State()
+    s_edited   = State()
+    s_save     = State()
+
 
     t_idle = s_idle.to.itself()
 
-    t_create      = s_idle.to(s_create)
+    t_create      = s_idle.to(s_create, cond="is_create_cmd")
     t_create_done = s_create.to(s_inactive)
 
-    t_open      = s_idle.to(s_open_project)
+    t_open      = s_idle.to(s_open_project, cond="is_create_cmd")
     t_open_done = s_open_project.to(s_inactive)
 
-    s_inactive = s_inactive.to.itself()
+    t_inactive = s_inactive.to.itself()
+
+    t_set_active   = s_inactive.to(s_active, cond="is_create_cmd")
+    t_set_inactive = s_active.to(s_inactive)
+
+    t_active = s_active.to.itself()
+
+    t_set_active   = s_active.to(s_edited)
+
+    t_edited = s_edited.to.itself()
+
+    t_save      = s_edited.to(s_save)
+    t_save_done = s_save.to(s_active)
 
     t_close      = s_inactive.to(s_close)
     t_close_done = s_close.to(s_idle)
@@ -26,7 +42,24 @@ class FSMProjectManager(StateMachine):
     t_delete      = s_inactive.to(s_delete)
     t_delete_done = s_delete.to(s_idle)
 
-    t_save      = s_inactive.to(s_delete)
-    t_save_done = s_delete.to(s_idle)
+    async def is_create_cmd(self, cmd: CmdProject):
+        return cmd.subcmd == CmdProject.SubCmdProject.CREATE
 
-class SubFSMProjectStatus(StateMachine):
+    async def is_open_cmd(self, cmd: CmdProject):
+        return cmd.subcmd == CmdProject.SubCmdProject.OPEN
+
+    async def is_save_cmd(self, cmd: CmdProject):
+        return cmd.subcmd == CmdProject.SubCmdProject.SAVE
+
+    async def is_delete_cmd(self, cmd: CmdProject):
+        return cmd.subcmd == CmdProject.SubCmdProject.DELETE
+
+    async def is_close_cmd(self, cmd: CmdProject):
+        return cmd.subcmd == CmdProject.SubCmdProject.CLOSE
+
+    async def is_setactive_cmd(self, cmd: CmdProject):
+        return cmd.subcmd == CmdProject.SubCmdProject.SETACTIVE
+
+    async def is_unsetactive_cmd(self, cmd: CmdProject):
+        return cmd.subcmd == CmdProject.SubCmdProject.UNSETACTIVE
+
